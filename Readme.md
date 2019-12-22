0a830cae185800729521e170c222ad894ae34e5b
rishi <rishisingh0831@gmail.com>
2019-12-22 19:22:33 +0530

    App ready with local storage

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# FIOB
+
+A basic TO DO app.
+
+![screenshot](./assets/pic1.jpg)
+
+![screenshot](./assets/pic2.jpg)
+
+## How To Run
+
+In the project directory, you can run:
+
+### `yarn install`
+
+### `expo start`
--- app/Main.js
+++ app/Main.js
@@ -10,7 +10,7 @@ import {
 } from "react-native";
 import { OutlinedTextField } from "react-native-material-textfield";
 import { Card, Button, Input } from "react-native-elements";
-// import { AsyncStorage } from "react-native";
+import { AsyncStorage } from "react-native";
 
 const screenWidth = Dimensions.get("screen").width;
 const screenHeight = Dimensions.get("screen").height;
@@ -23,155 +23,87 @@ var date =
   "/" +
   today.getFullYear();
 
-var taskNo = 0;
-
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      task: "This is an example task and you can't delete it.",
       taskList: [],
-      //added: false, //this checks of the the "done" button is pressed or not.
-      dataTyped: false //this checks if the keyboard enter key is pressed or not.
-      //   savedDataRetriverd: [],
-      //   task1Created: false, //this check if the first task is created ot not.
-      //   taskNo: taskNo,
-      //   finalData: ""
+      taskNo: 0,
+      deletedTaskList: []
     };
   }
 
   componentDidMount() {
-    console.log("component did mount2");
-    // this.retriveTaskNo();
-    // this.retriveTask1Created();
+    console.log("component did mount");
+    this.retriveTasks();
+    console.log("component did mount end");
     // this.retriveSavedData();
   }
 
-  //   retriveTaskNo = async () => {
-  //     try {
-  //       const value = await AsyncStorage.getItem("taskNumber");
-  //       if (value !== null) {
-  //         // We have data!!
-  //         console.log(value);
-  //         this.setState({ taskNo: parseInt(value, 10) });
-  //       }
-  //     } catch (error) {
-  //       // Error retrieving data
-  //       console.log("Errod in retriving task no");
-  //     }
-  //   };
+  retriveTasks = async () => {
+    try {
+      const value = await AsyncStorage.getItem("allData18");
+      if (value !== null) {
+        // We have data!!
+        var retrivedData = JSON.parse(value);
+        console.log(retrivedData, "Data retrived");
+        this.setState({ taskList: retrivedData });
+        this.setState({ taskList: this.state.taskList });
+        var taskNo = retrivedData[retrivedData.length - 1].taskNo + 1;
+        this.setState({ taskNo: taskNo });
+      }
+    } catch (error) {
+      // Error retrieving data
+      console.log("Error in retriving saved data");
+    }
+    if (1 === 1) {
+      try {
+        const value = await AsyncStorage.getItem("deletedData1");
+        if (value !== null) {
+          // We have data!!
+          var retrivedData = JSON.parse(value);
+          console.log(retrivedData, "Deleted Data retrived");
+          this.setState({ deletedTaskList: retrivedData });
+          this.setState({ deletedTaskList: this.state.deletedTaskList });
+        }
+      } catch (error) {
+        // Error retrieving data
+        console.log("Error in retriving deleted data");
+      }
+    }
+  };
 
-  //   retriveTask1Created = async () => {
-  //     try {
-  //       const value = await AsyncStorage.getItem("task1created");
-  //       if (value !== null) {
-  //         // We have data!!
-  //         console.log(Boolean(value));
-  //         this.setState({ task1Created: Boolean(value) });
-  //       }
-  //     } catch (error) {
-  //       // Error retrieving data
-  //       console.log("Error in retriving saved data");
-  //     }
-  //     this.retriveSavedData();
-  //   };
-
-  //   retriveSavedData = async () => {
-  //     if (this.state.task1Created) {
-  //       try {
-  //         const value = await AsyncStorage.getItem("allData2");
-  //         if (value !== null) {
-  //           // We have data!!
-  //           console.log("component did mount1");
-  //           var retrivedData = JSON.parse(value);
-  //           this.setState({ savedDataRetriverd: retrivedData });
-  //         }
-  //       } catch (error) {
-  //         // Error retrieving data
-  //       }
-  //     } else {
-  //       console.log(this.state.task1Created);
-  //     }
-  //   };
-
-  onSubmit = () => {
+  onSubmit = async () => {
     let { current: field } = this.fieldRef;
+    var taskNo = this.state.taskNo;
     var task = {
       task: field.value(),
-      taskNo: taskNo++,
+      taskNo: taskNo,
       date: date,
       isComplete: false
     };
     var newTaskList = this.state.taskList.concat(task);
     this.setState({ taskList: newTaskList });
-    this.setState({ added: !this.state.added });
-    this.setState({ dataTyped: false });
     console.log(this.state.taskList);
-  };
-
-  showList = async () => {
-    console.log(this.state.taskList);
+    this.setState({ taskNo: this.state.taskNo + 1 });
 
     // storing the data in local storage
 
-    // var dataToBeSaved = this.state.savedDataRetriverd.concat(
-    //   this.state.taskList
-    // );
+    var dataToBeSaved = JSON.stringify(newTaskList);
 
-    // try {
-    //   await AsyncStorage.setItem("allData2", JSON.stringify(dataToBeSaved));
-    // } catch (error) {
-    //   // Error saving data
-    //   console.log("error in saving data");
-    // }
-
-    // // storing the "task1created" state in local storage.
-    // try {
-    //   await AsyncStorage.setItem(
-    //     "task1created",
-    //     JSON.stringify(this.state.task1Created)
-    //   );
-    // } catch (error) {
-    //   // Error saving data
-    //   console.log("error in saving task1created");
-    // }
-
-    // // storing the "taskNo" state in local storage.
-    // try {
-    //   await AsyncStorage.setItem("taskNumber", taskNo.toString());
-    // } catch (error) {
-    //   // Error saving data
-    //   console.log("error in saving taskNumber");
-    // }
-
-    // // retriving data from local storage
-
-    // var finalData;
-
-    // try {
-    //   const value = await AsyncStorage.getItem("allData2");
-    //   if (value !== null) {
-    //     // We have data!!
-    //     console.log(JSON.parse(value));
-    //     finalData = JSON.parse(value);
-    //   }
-    // } catch (error) {
-    //   // Error retrieving data
-    // }
-
-    // this.setState({
-    //   added: !this.state.added,
-    //   dataTyped: false
-    //   task1Created: true,
-    //   finalData: finalData
-    // });
-    // console.log(this.state.finalData);
+    try {
+      await AsyncStorage.setItem("allData18", dataToBeSaved);
+      console.log("no error in saving data");
+    } catch (error) {
+      // Error saving data
+      console.log("error in saving data");
+    }
   };
 
   fieldRef = React.createRef();
 
   render() {
-    renderTasks = this.state.taskList
+    renderDeletedTasks = this.state.deletedTaskList
       .slice(0)
       .reverse()
       .map(tasks => {
@@ -189,17 +121,98 @@ class Main extends React.Component {
                 title="Delete"
                 titleStyle={{ color: "red" }}
                 type="clear"
-                onPress={() => {
-                  for (var i = 0; i < this.state.taskList.length; i++) {
-                    if (this.state.taskList[i].taskNo == tasks.taskNo) {
-                      this.state.taskList.splice(i, 1);
-                      this.setState({ taskList: this.state.taskList });
+                onPress={async () => {
+                  for (var i = 0; i < this.state.deletedTaskList.length; i++) {
+                    if (this.state.deletedTaskList[i].taskNo == tasks.taskNo) {
+                      this.state.deletedTaskList.splice(i, 1);
+                      this.setState({
+                        deletedTaskList: this.state.deletedTaskList
+                      });
+
+                      var dataToBeSaved = JSON.stringify(
+                        this.state.deletedTaskList
+                      );
+
+                      try {
+                        await AsyncStorage.setItem(
+                          "deletedData1",
+                          dataToBeSaved
+                        );
+                        console.log("no error in deleting deleted data");
+                      } catch (error) {
+                        // Error saving data
+                        console.log("error in deleting deleted data");
+                      }
                     }
                   }
                 }}
               />
             </Card>
             <Text></Text>
+          </View>
+        );
+      });
+
+    renderTasks = this.state.taskList
+      .slice(0)
+      .reverse()
+      .map(tasks => {
+        return (
+          <View key={tasks.taskNo}>
+            <Card
+              title={tasks.date}
+              containerStyle={{
+                borderRadius: 30,
+                borderColor: "#f2f7ff"
+              }}
+            >
+              <Text style={styles.text}>{tasks.task}</Text>
+              <Button
+                title="Complete"
+                titleStyle={{ color: "red" }}
+                type="clear"
+                onPress={async () => {
+                  for (var i = 0; i < this.state.taskList.length; i++) {
+                    if (this.state.taskList[i].taskNo == tasks.taskNo) {
+                      var deletedTask = this.state.taskList[i];
+                      this.setState({
+                        deletedTaskList: this.state.deletedTaskList.concat(
+                          deletedTask
+                        )
+                      });
+                      this.state.taskList.splice(i, 1);
+                      this.setState({ taskList: this.state.taskList });
+
+                      var dataToBeSaved = JSON.stringify(this.state.taskList);
+
+                      try {
+                        await AsyncStorage.setItem("allData18", dataToBeSaved);
+                        console.log("no error in saving data after deletion");
+                      } catch (error) {
+                        // Error saving data
+                        console.log("error in saving data after deletion");
+                      }
+
+                      if (1 === 1) {
+                        var deletedDataToBeSaved = JSON.stringify(
+                          this.state.deletedTaskList
+                        );
+                        try {
+                          await AsyncStorage.setItem(
+                            "deletedData1",
+                            deletedDataToBeSaved
+                          );
+                          console.log("no error in saving deleted data");
+                        } catch (error) {
+                          // Error saving data
+                          console.log("error in saving deleted data");
+                        }
+                      }
+                    }
+                  }
+                }}
+              />
+            </Card>
             <Text></Text>
           </View>
         );
@@ -253,10 +266,16 @@ class Main extends React.Component {
               <Text></Text>
               <Text></Text>
               <Text></Text>
+              <Text style={styles.boldText}>Your tasks will appear below.</Text>
+              <Text></Text>
               {renderTasks}
-              <Text style={styles.text}>Your tasks will appear here.</Text>
               <Text></Text>
               <Text></Text>
+              <Text style={styles.boldText}>
+                Your completed tasks will appear below.
+              </Text>
+              <Text></Text>
+              {renderDeletedTasks}
               <Text></Text>
               <Text></Text>
             </View>
@@ -290,7 +309,16 @@ const styles = StyleSheet.create({
     marginRight: 30,
     lineHeight: 30,
     letterSpacing: 1
-    // textAlign: 'center',
+  },
+  boldText: {
+    fontFamily: "Roboto",
+    fontSize: 17,
+    color: "#000",
+    marginLeft: 30,
+    marginRight: 30,
+    lineHeight: 30,
+    letterSpacing: 1,
+    fontWeight: "bold"
   },
   date: {
     fontFamily: "Roboto",
@@ -305,11 +333,9 @@ const styles = StyleSheet.create({
     flexGrow: 1
   },
   titleStyle: {
-    // fontFamily: 'serif',
     fontSize: 35,
     color: "white",
     fontWeight: "bold",
-    // marginRight: 80,
     marginTop: 20
   }
 });
